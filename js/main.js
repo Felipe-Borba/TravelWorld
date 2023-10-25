19b733bbc8031b5fee6bf28a27d531d685b9100e
Felipe Borba <felipe.borba0109@gmail.com>
2023-10-24 21:33:00 -0300

    agora vai

--- js/main.js
+++ js/main.js
@@ -4,11 +4,10 @@ const galleryImage = document.getElementById("imgGallery");
 
 let imageIndex = 0;
 const images = [
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdRsdUBoWqeSvYC-yOADKwy9MBC1e1vunsjkYfZz37&s",
-  "https://static.vecteezy.com/ti/fotos-gratis/t2/6026259-tranquilo-verao-ferias-ferias-paisagem-tropical-ilha-por-do-sol-praia-palms-shore-calmo-mar-areia-exotico-natureza-cenario-inspirador-e-pacifico-paisagem-marinha-reflexo-ceu-por-do-sol-foto.jpg",
   "https://all.accor.com/magazine/imagerie/ferias-na-praia-o-que-fazer-das-f163.jpg",
   "https://media.istockphoto.com/id/1300296030/pt/foto/tropical-beach-palm-trees-sea-wave-and-white-sand.jpg?s=612x612&w=0&k=20&c=nHjLozpdgxS4FJHDMrq-W-Vm8z3JrTo-C8WKoaLtpao=",
 ];
+galleryImage.setAttribute("src", images[0]);
 
 rightBtn.addEventListener("click", () => {
   let next = imageIndex + 1;
